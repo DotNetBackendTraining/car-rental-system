d97cc5f3f714d78c7e33759f8b0095598ef4cc5d
Izzat Alsharif <work@izzatalsharif.com>
2024-07-11 22:05:14 +0300

    Add ApplicationUserProfile

--- CarRentalSystem.Web/DependencyInjection/PresentationServiceCollectionExtension.cs
+++ CarRentalSystem.Web/DependencyInjection/PresentationServiceCollectionExtension.cs
@@ -1,4 +1,6 @@
+using System.Reflection;
 using CarRentalSystem.Web.Filters;
+using CarRentalSystem.Web.Profiles;
 using CarRentalSystem.Web.ViewModels.Validators;
 using FluentValidation;
 
@@ -8,6 +10,7 @@ public static class PresentationServiceCollectionExtension
 {
     public static void AddPresentationServices(this IServiceCollection services)
     {
+        services.AddAutoMapper(Assembly.GetAssembly(typeof(ApplicationUserProfile)));
         services.AddValidatorsFromAssemblyContaining<SignUpViewModelValidator>();
         services.AddScoped(typeof(ValidationFilter<>));
         services.AddRazorPages();
