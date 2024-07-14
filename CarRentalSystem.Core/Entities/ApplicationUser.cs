26605cd6be459fc53e891789b5be733efa4c0a89
Izzat Alsharif <work@izzatalsharif.com>
2024-07-14 21:30:27 +0300

    Fix nullable

--- CarRentalSystem.Core/Entities/ApplicationUser.cs
+++ CarRentalSystem.Core/Entities/ApplicationUser.cs
@@ -8,7 +8,7 @@ public class ApplicationUser : IdentityUser
     public string LastName { get; set; } = string.Empty;
     public DateTime? DateOfBirth { get; set; }
     public string AddressLine1 { get; set; } = string.Empty;
-    public string AddressLine2 { get; set; } = string.Empty;
+    public string? AddressLine2 { get; set; } = string.Empty;
     public string City { get; set; } = string.Empty;
     public string Country { get; set; } = string.Empty;
     public string DriversLicenseNumber { get; set; } = string.Empty;
