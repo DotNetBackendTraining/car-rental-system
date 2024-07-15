668216c017a2ddae3f4ced44f85dcdcbf23edfd9
Izzat Alsharif <work@izzatalsharif.com>
2024-07-15 14:42:58 +0300

    Authorize find page

--- CarRentalSystem.Web/Controllers/FindController.cs
+++ CarRentalSystem.Web/Controllers/FindController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarRentalSystem.Web.Controllers;
 
+[Authorize]
 public class FindController : Controller
 {
     public IActionResult Index()
