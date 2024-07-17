2e2038130fb2947f61008e6e15919bc3a2127a07
Izzat Alsharif <work@izzatalsharif.com>
2024-07-17 17:33:17 +0300

    Add AbstractController to handle errors

---
+++ CarRentalSystem.Web/Controllers/AbstractController.cs
@@ -0,0 +1,27 @@
+﻿using CarRentalSystem.Core.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalSystem.Web.Controllers;
+
+public abstract class AbstractController : Controller
+{
+    protected void HandleErrors(Result result)
+    {
+        if (result.IsSuccess)
+        {
+            throw new InvalidOperationException(nameof(HandleErrors) + " must not be called on success");
+        }
+
+        if (result is IValidationResult validationResult)
+        {
+            foreach (var error in validationResult.Errors)
+            {
+                ModelState.AddModelError(error.Code, error.Message);
+            }
+        }
+        else
+        {
+            ModelState.AddModelError(result.Error.Code, result.Error.Message);
+        }
+    }
+}
